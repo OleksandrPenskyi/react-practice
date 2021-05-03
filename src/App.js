9175cb5fda04f84f9638667ec701d3cb0d7622cc
OleksandrPenskyi <suchgoodidea@gmail.com>
2021-05-03 22:39:17 +0300

    added solve for 1st task

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+const App = () => <Counter startValue={12} />;
+
+export default App;
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import style from './Counter.module.css';
+import PropTypes from 'prop-types';
+
+class Counter extends Component {
+  static defaultProps = {
+    startValue: 0,
+  };
+
+  static propTypes = {
+    startValue: PropTypes.number,
+  };
+
+  state = {
+    value: this.props.startValue,
+  };
+
+  onIncrementClick = () => {
+    this.setState(previewState => ({
+      value: previewState.value + 1,
+    }));
+  };
+
+  onDecrementClick = () => {
+    this.setState(previewState => ({
+      value: previewState.value - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div className={style.wrapper}>
+        <p>
+          Счетчиик:
+          <span>{this.state.value}</span>
+        </p>
+
+        <div>
+          <button
+            className={style.btn}
+            type="button"
+            onClick={this.onIncrementClick}
+          >
+            Увелчиваем на 1
+          </button>
+          <button
+            className={style.btn}
+            type="button"
+            onClick={this.onDecrementClick}
+          >
+            Уменьшаем на 1
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
