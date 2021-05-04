666dfeca90917fb8046df552b6aa19fae50f8dec
OleksandrPenskyi <suchgoodidea@gmail.com>
2021-05-04 18:32:19 +0300

    added resolve for 4th task

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Switcher from './components/Switcher';
 import Counter from './components/Counter';
 import ColorPicker from './components/ColorPicker';
+import TodoList from './components/TodoList';
 
-import colorPicker from './data/colorPicker.json';
+import data from './data';
+const { todos, colorPicker } = data;
 
-const App = () => (
-  <>
-    <Counter startValue={12} />
-    <Switcher />
-    <ColorPicker options={colorPicker} />
-  </>
-);
+class App extends Component {
+  state = {
+    todoList: todos,
+  };
+
+  deleteTodoItem = id => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.filter(item => item.id !== id),
+    }));
+  };
+
+  render() {
+    const { todoList } = this.state;
+    return (
+      <>
+        <Counter startValue={12} />
+        <Switcher />
+        <ColorPicker options={colorPicker} />
+        <TodoList todo={todoList} deleteTodoItem={this.deleteTodoItem} />
+      </>
+    );
+  }
+}
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+import colorPicker from './colorPicker.json';
+import todos from './todos.json';
+
+const data = { colorPicker, todos };
+export default data;
